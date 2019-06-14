0b46daa5ac573b5b7c06e9a274fbf2d34bee95a2
Jonatas Souza <jonatas@easycomtec.com>
2019-06-14 16:50:45 -0300

    Variaveis de ambiente

--- src/pages/Feed.js
+++ src/pages/Feed.js
@@ -22,7 +22,7 @@ class Feed extends Component {
     }
 
     registerToSocket = () => {
-        const socket = io('http://192.168.0.103:3333');
+        const socket = io(process.env.REACT_APP_API_URL);
 
         socket.on('post', newPost => {
             this.setState({ feed: [newPost, ...this.state.feed ]});
@@ -55,7 +55,7 @@ class Feed extends Component {
                             <img src={more} alt='Mais' />
                         </header>
                         
-                        <img src={`http://192.168.0.103:3333/files/${post.image}`} alt='' />
+                        <img src={`${process.env.REACT_APP_API_URL}/files/${post.image}`} alt='' />
 
                         <footer>
                             <div className='actions'>
