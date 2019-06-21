b4dfae79ae32acd603c918a5956b052ceb2788d3
Jonatas Souza <jonatas@easycomtec.com>
2019-06-21 19:31:34 -0300

    PWA

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import Header from './components/Header';
-import Body from './components/Body';
-import { Provider } from 'react-redux';
-import store from './store';
-import 'react-notifications/lib/notifications.css';
-import { NotificationContainer } from 'react-notifications';
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import Header from "./components/Header";
+import Body from "./components/Body";
+import { Provider } from "react-redux";
+import store from "./store";
+import { NotificationContainer } from "react-notifications";
+import "react-notifications/lib/notifications.css";
 
 function App() {
   return (
